7b9d699091adfe842a374353b6720e6583265243
codesourse <1135768777@qq.com>
2014-08-22 17:19:08 +0800

    test

---
+++ RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  BoxPlay
+//
+//  Created by 夏科杰 on 14-7-28.
+//  Copyright (c) 2014年 夏科杰. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface RootViewController : GLKViewController
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BoxPlay
+//
+//  Created by 夏科杰 on 14-7-28.
+//  Copyright (c) 2014年 夏科杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
